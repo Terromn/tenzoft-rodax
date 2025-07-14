8f95445faae72769af5e64a3a5fc86c48ed95144
terromn <terromn@users.noreply.github.com>
2025-07-14 06:04:40 -0500

    feat: add node in astro config and proyect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from "@astrojs/node";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  server: {
+    host: '0.0.0.0'
+  },
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
